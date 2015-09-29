dcc4031fded1cebfc4a88308478d4f61b47f3650
Miguel Ramirez Jr <derock8888@gmail.com>
2015-09-28 23:43:36 -0400

    final commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,33 @@
-var models = require('models');
-var views = require('views');
+//var models = require('models');
+var Likes = require('models/likes');
+window.likes = new Likes ();
 
 $(document).ready(function(){
   $('body').prepend(JST.application());
 
-  var view = new PostsView();
-
-  $(document).on('posts:fetched', function(event, posts){
-    view.showPosts(posts);
+  $("#addbutton").on('click', function() {
+      likes.increase();
+      $("#addbutton").text(likes.get('count') +  "Likes")
   });
 
-  Post.fetch();
 });
+
+module.exports = Likes;
+
+
+
+// var PersonCollection = require('models/person-collection');
+//
+// $(document).ready(function(){
+//   // prepend the contents of `app/templates/application.hbs` into `body`
+//   $('#container').append(JST.application());
+//
+//   var people = new PersonCollection([
+//     {name: "Jacob Smith", email: "jake@theironyard.com"},
+//     {name: "Mason Stewart"}
+//   ]);
+//
+//   people.each(function(person){
+//     $('.people').append(JST.person(person.toJSON()));
+//   });
+// });
